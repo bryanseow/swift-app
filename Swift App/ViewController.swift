90f83d883f4285c974223d6f54c7263f7f0bf18e
Bryan Seow <bryan.seow@gmail.com>
2017-06-02 22:12:05 +1000

    Learning about text fields

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -11,15 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
-   
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
+    
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount = tapCount + 1
-        
-        if tapCount >= 20 {
-            theLabel.text = "You tapped the button 20 times!"
-        }
+        print(text1.text!)
+        print(text2.text!)
     }
     
     @IBAction func buttonChange(_ sender: Any) {
